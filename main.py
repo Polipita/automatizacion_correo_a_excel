fb575f0aff7f1b8f2a919e35dd77e0985b992913
Blas Alejandro Calderon <116234502+Polipita@users.noreply.github.com>
2026-01-21 15:57:40 -0500

    confirmando fecha

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ def procesar_correo():
     mail = imaplib.IMAP4_SSL(IMAP_SERVER)
     mail.login(EMAIL, PASSWORD)
     mail.select("inbox")
-
-    status, mensajes = mail.search(None, f'(UNSEEN ON {hoy})')
+    #por ahora solo es de hoy para evitar vovler cargar facturas anteriores
+    status, mensajes = mail.search(None, f'(UNSEEN ON{hoy})')
     #[b'21 22 23']
     ids_correos = mensajes[0].split()
                 #b'21 22 23' => [b'21',b'22',b'23']
@@ -64,7 +64,7 @@ def procesar_correo():
         print(f"Procesando: {asunto} | De: {from_raw}")
 
         if es_remitente_valido:
-            print("  ✅ APROBADO: Cumple filtros de seguridad.")
+            print("cumple los filtros")
             
             archivo_encontrado = False
             for parte in mensaje.walk():
@@ -78,14 +78,14 @@ def procesar_correo():
                         ruta = os.path.join(CARPETA, nombre_archivo)
                         with open(ruta, "wb") as f:
                             f.write(parte.get_payload(decode=True))
-                        print(f"  📥 PDF Guardado: {nombre_archivo}")
+                        print(f"se guarda el pdf : {nombre_archivo}")
                         archivo_encontrado = True
             
             if archivo_encontrado:
                 # Solo marcamos como LEÍDO si guardamos algo exitosamente
                 mail.store(num, '+FLAGS', '\\Seen')
         else:
-            print("  ❌ IGNORADO: No es de confianza o no es una factura.")
+            print("no es correo de confianza")
 
     mail.logout()
     
